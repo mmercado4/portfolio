b1c56293949db7bdfe20a3dfaa4d45580e49c64b
Miguel <user@MacBook-Pro-de-USER.local>
2024-10-17 19:20:26 +0200

    config netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  output: 'server',
+  adapter: netlify()
 });
