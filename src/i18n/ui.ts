59482d47742fce1d53bc06b78dc512765d53e97e
Miguel <user@MacBook-Pro-de-USER.local>
2024-10-19 14:43:34 +0200

    improve sections and add skills

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,7 +11,11 @@ export const ui = {
     "Hi! I'm Miguel": "Hi! I'm Miguel",
     '{years} years experienced Full Stack Developer': '{years} years experienced Full Stack Developer',
     'Addicted to software development and building new things.':
-      'Addicted to software development and building new things.'
+      'Addicted to software development and building new things.',
+    'Work experience': 'Work experience',
+    'About me': 'About me',
+    Education: 'Education',
+    Skills: 'Skills'
   },
   es: {
     'Hello!': 'Hola',
@@ -19,6 +23,10 @@ export const ui = {
     '{years} years experienced Full Stack Developer':
       'Desarrollador Full Stack con {years} años de experiencia',
     'Addicted to software development and building new things.':
-      'Adicto al desarrollo de software y a crear cosas nuevas.'
+      'Adicto al desarrollo de software y a crear cosas nuevas.',
+    'Work experience': 'Experiencia profesional',
+    'About me': 'Sobre mi',
+    Education: 'Educación',
+    Skills: 'Habilidades'
   }
 } as const;
